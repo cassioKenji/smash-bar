3cb76b36a1f32820b87483d38612ead9c061463b
Cássio Kenji <cassio.oshiro@walmart.com>
2016-12-08 22:37:30 -0200

    init commit for an Unity project

---
+++ Assets/Scripts/BarFill.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BarFill : MonoBehaviour {
+	[SerializeField]
+	private float fillAmount;
+
+	[SerializeField]
+	private Image content;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		if (Input.GetKeyDown ("space"))
+			FillIncrease ();
+		
+		UpdateBar ();
+		FillDecrease ();
+	}
+
+	private void UpdateBar(){
+		content.fillAmount = fillAmount;
+	}
+
+	private void FillIncrease(){
+		if(fillAmount < 1.0f)
+			fillAmount += 0.1f;
+	}
+	private void FillDecrease(){
+		if(fillAmount > 0f)
+			fillAmount -= 0.1f/0.5f * Time.deltaTime;
+	}
+	public float GetFill(){
+		float fill = content.fillAmount;
+		return fill;
+	}
+}
---
+++ Assets/Scripts/BarPowerConvert.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BarPowerConvert : MonoBehaviour{
+
+	private BarFill bf;
+
+	void Awake(){
+	
+		bf = GetComponent<BarFill> ();
+	}
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown ("space"))
+			print(FillConvert ());	
+	}
+
+	private int FillConvert (){
+		
+		float rawFill = bf.GetFill ();
+		int intFill = (int)rawFill * 10;
+		return intFill * 10;
+	}
+}
